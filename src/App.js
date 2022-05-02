5f28c2b1c33a4b24f3430fde222724a7913b0f88
NishiKadlera <nishikadlera@gmail.com>
2022-05-02 11:22:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import logo from './assets/react.jpg';
-import './App.css';
-import Functioncomponent from './components/Functioncomponent';
-import ClassCompBasic from './components/classcomponent';
-import Details from './components/nestedcomp';
-import emp from './Data/EmpDetails.json';
-import Comp from './components/Comp';
+import logo from "./assets/react.jpg";
+import "./App.css";
+import Functioncomponent from "./components/Functioncomponent";
+import ClassCompBasic from "./components/classcomponent";
+import Details from "./components/nestedcomp";
+import emp from "./Data/EmpDetails.json";
+import Comp from "./components/Comp";
 
 function App() {
-  return(
-  
+  return (
     <div className="App">
       <header className="App-header">
-        <h1 id="react"><img></img> REACT</h1><br></br><br></br><br></br>
-        <img src={logo} className="App-logo" alt="logo" /><br></br><br></br><br></br>
+        <h1 id="react">
+          <img></img> REACT
+        </h1>
+        <br></br>
+        <br></br>
+        <br></br>
+        <img src={logo} className="App-logo" alt="logo" />
+        <br></br>
+        <br></br>
+        <br></br>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -25,14 +32,14 @@ function App() {
           Hello Everyone! Welcome to React.
         </a>
       </header>
-      
+
       <div className="wrapper">
         <h1>Welcome to Functional Component</h1>
-        <Functioncomponent/>
-        <h1>Welcome to Class Component</h1>
-        <ClassCompBasic/>
+        <Functioncomponent />
+        <h1>Welcome to className Component</h1>
+        <ClassCompBasic />
         <h1>Welcome to Nested component</h1>
-        <Details/>
+        <Details />
         <h1>Welcome to Comp</h1>
         <Comp></Comp>
         <h3>List of Employees</h3>
@@ -51,58 +58,48 @@ function App() {
           </thead>
 
         </table> */}
-        <div class="container">
-  <div class="row">
-    <div class="col-md-2">
-      <h3>Photo</h3>
-    </div>
-    <div class="col-md-2">
-    <h3>ID</h3>
-    </div>
-    <div class="col-md-2">
-    <h3>Name</h3>
-    </div>
-    <div class="col-md-2">
-    <h3>Designation</h3>
-    </div>
-    <div class="col-md-2">
-    <h3>Age</h3>
-    </div>
-    <div class="col-md-2">
-    <h3>Score</h3>
-    </div>
-  </div>
-</div> 
-<hr></hr>   
-        { 
-        emp.map((record, i) =>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-2">
+              <h3>Photo</h3>
+            </div>
+            <div className="col-md-2">
+              <h3>ID</h3>
+            </div>
+            <div className="col-md-2">
+              <h3>Name</h3>
+            </div>
+            <div className="col-md-2">
+              <h3>Designation</h3>
+            </div>
+            <div className="col-md-2">
+              <h3>Age</h3>
+            </div>
+            <div className="col-md-2">
+              <h3>Score</h3>
+            </div>
+          </div>
+        </div>
+        <hr></hr>
+        {emp.map((record, i) => (
           <div key={i}>
-
             {/* <img src={record.path}  /> */}
-            <div class="container">
-  <div class="row">
-  <div class="col-md-2">
-    <img src={record.icon} width="50px" height="50px"/>
-    </div><br></br>
-    <div class="col-md-2">
-    {record.id}
-    </div>
-    <div class="col-md-2">
-    {record.Name}
-    </div>
-    <div class="col-md-2">
-    {record.Designation}
-    </div>
-    <div class="col-md-2">
-    {record.Age}
-    </div>
-    <div class="col-md-2">
-    {record.Score}
-    </div>
-  </div>
-  <hr></hr>
-</div>
-            {/* <table class="table">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-2">
+                  <img src={record.icon} width="50px" height="50px" />
+                </div>
+                <br></br>
+                <div className="col-md-2">{record.id}</div>
+                <div className="col-md-2">{record.Name}</div>
+                <div className="col-md-2">{record.Designation}</div>
+                <div className="col-md-2">{record.Age}</div>
+                <div className="col-md-2">{record.Score}</div>
+              </div>
+              <hr></hr>
+            </div>
+
+            {/* <table className="table">
               
               <tbody> 
                 <tr>
@@ -117,11 +114,10 @@ function App() {
           
             </table> */}
           </div>
-        )}
-
+        ))}
+      </div>
     </div>
-    </div>
-    );
+  );
 }
-    
+
 export default App;
